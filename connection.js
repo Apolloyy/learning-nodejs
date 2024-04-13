e00f6be4fc318806f1d41f891568fb26c7ba7f27
Lucas <luroyaaal@gmail.com>
2024-04-13 11:04:49 -0300

    :sparkles: todas as querys

--- connection.js
+++ connection.js
@@ -27,3 +27,63 @@ con.query("SELECT * FROM Pessoa;", (err, res) => {
   }
   console.log(res);
 });
+
+//insert
+const nm_pessoa = "Rafael";
+const nr_numero = 222222222;
+const sql = `INSERT INTO pessoa (nm_pessoa, nr_pessoa) VALUES (?, ?)`;
+con.query(sql, [nm_pessoa, nr_numero], (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("Inserção bem sucedida!");
+});
+
+//delete
+const id_delete = 9;
+const sqlDelete = `DELETE FROM pessoa WHERE id_pessoa = ?`;
+con.query(sqlDelete, [id_delete], (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("Deletado");
+});
+
+//update
+const id_update = 10;
+const new_nm_pessoa = "Apollo";
+const sqlUpdate = `UPDATE pessoa SET nm_pessoa = ? WHERE id_pessoa = ?`;
+con.query(sqlUpdate, [new_nm_pessoa, id_update], (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("Atualizado");
+});
+
+//select with where
+con.query(
+  "SELECT * FROM Pessoa WHERE id_pessoa = ?;",
+  [id_delete],
+  (err, res) => {
+    if (err) {
+      console.error("🗙 Erro ao executar a consulta:", err);
+      return;
+    }
+    console.log(res);
+  }
+);
+
+con.query(
+  "SELECT * FROM Pessoa WHERE id_pessoa = ?;",
+  [id_update],
+  (err, res) => {
+    if (err) {
+      console.error("🗙 Erro ao executar a consulta:", err);
+      return;
+    }
+    console.log(res);
+  }
+);
